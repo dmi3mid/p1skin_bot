994ef48506e7910ad95effaf3748efa2934dde78
dmi3mid <dmi3midd@gmail.com>
2025-03-27 22:55:35 +0200

    Make module "fetching" with fetch api

--- bot.js
+++ bot.js
@@ -1,24 +1,25 @@
 const Tg = require('node-telegram-bot-api');
 require('dotenv').config();
+const {getFetch} = require('./fetching.js');
 const bot = new Tg(process.env.TOKEN, {polling: true});
 
-async function getResults(url = 'http://localhost:2800/results') {
-    try {
-        let response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
-        })
-        if (!response.ok) {
-            throw new Error(`Помилка запиту: ${response.statusText}`);
-        }
-        let result = await response.json();
-        return result   
-    } catch (error) {
-        console.error('Помилка запиту', error)
-    }
-}
+// async function getResults(url = 'http://localhost:2800/results') {
+//     try {
+//         let response = await fetch(url, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json;charset=utf-8'
+//             }
+//         })
+//         if (!response.ok) {
+//             throw new Error(`Помилка запиту: ${response.statusText}`);
+//         }
+//         let result = await response.json();
+//         return result   
+//     } catch (error) {
+//         console.error('Помилка запиту', error)
+//     }
+// }
 
 bot.onText("/start", async (msg) => {
     const chatId = msg.chat.id;
@@ -32,6 +33,6 @@ bot.on("voice", async (msg) => {
 });
 
 bot.on("inline_query", async (query) => {
-    const results = await getResults();
+    const results = await getFetch();
     await bot.answerInlineQuery(query.id, results);
 });
