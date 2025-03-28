732b6d17d93047ed3c7f6c30b362fc33de5786c8
dmi3mid <dmi3midd@gmail.com>
2025-03-28 09:11:16 +0200

    local storage for voices

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const Tg = require('node-telegram-bot-api');
 require('dotenv').config();
 
-const {getFetch} = require('./fetching.js');
+const { promiseReadFile } = require('./files.js');
 
 const bot = new Tg(process.env.TOKEN, {polling: true});
 
@@ -9,12 +10,13 @@ bot.onText("/start", async (msg) => {
     const chatId = msg.chat.id;
     await bot.sendMessage(chatId, "Привіт! Я бот для відправки голосових повідомлень");
 });
+const pathToVoices = path.join(__dirname, "voices.json");
+bot.on("inline_query", async (query) => {
+    const results = await promiseReadFile(pathToVoices);
+    await bot.answerInlineQuery(query.id, results);
+});
 bot.on("voice", async (msg) => {
     const chatId = msg.chat.id;
     const voiceId = msg?.voice?.file_id;
     await bot.sendMessage(chatId, `Voice id:\n<code>${voiceId}</code>`, { parse_mode: "HTML" });
-});
-bot.on("inline_query", async (query) => {
-    const results = await getFetch();
-    await bot.answerInlineQuery(query.id, results);
 });
